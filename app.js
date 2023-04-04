572fe8c53c5bd6061e99c24e7861aa39657d52fb
hanifomarr <57039713+hanifomarr@users.noreply.github.com>
2023-04-04 12:02:38 +0800

    Error validation

--- app.js
+++ app.js
@@ -28,61 +28,97 @@ app.get("/", (req, res) => {
   res.render("homepage");
 });
 
-app.get("/foodtruck", async (req, res) => {
-  const foodtrucks = await Foodtruck.find({});
-  res.render("foodtrucks/index", { foodtrucks });
-});
+app.get(
+  "/foodtruck",
+  CatchAsync(async (req, res) => {
+    const foodtrucks = await Foodtruck.find({});
+    res.render("foodtrucks/index", { foodtrucks });
+  })
+);
 
-app.get("/foodtruck/new", async (req, res) => {
+app.get("/foodtruck/new", (req, res) => {
   res.render("foodtrucks/new");
 });
 
-app.post("/foodtruck", (req, res) => {
-  const newFoodtruck = new Foodtruck(req.body.foodtruck);
-  newFoodtruck.save();
-  res.redirect(`/foodtruck/${newFoodtruck._id}`);
+app.post(
+  "/foodtruck",
+  CatchAsync(async (req, res, next) => {
+    const newFoodtruck = new Foodtruck(req.body.foodtruck);
+    await newFoodtruck.save();
+    res.redirect(`/foodtruck/${newFoodtruck._id}`);
+  })
+);
+
+app.get(
+  "/foodtruck/:id",
+  CatchAsync(async (req, res) => {
+    const { id } = req.params;
+    const foodtruck = await Foodtruck.findById(id).populate("reviews");
+    res.render("foodtrucks/show", { foodtruck });
+  })
+);
+
+app.get(
+  "/foodtruck/:id/edit",
+  CatchAsync(async (req, res) => {
+    const { id } = req.params;
+    const foodtruck = await Foodtruck.findById(id);
+    res.render("foodtrucks/edit", { foodtruck });
+  })
+);
+
+app.put(
+  "/foodtruck/:id",
+  CatchAsync(async (req, res) => {
+    const { id } = req.params;
+    const updateFoodtruck = await Foodtruck.findByIdAndUpdate(id, {
+      ...req.body.foodtruck,
+    });
+    res.redirect(`/foodtruck/${updateFoodtruck._id}`);
+  })
+);
+
+app.delete(
+  "/foodtruck/:id",
+  CatchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Foodtruck.findByIdAndDelete(id);
+    res.redirect("/foodtruck");
+  })
+);
+
+app.post(
+  "/foodtruck/:id/review",
+  CatchAsync(async (req, res) => {
+    const foodtruck = await Foodtruck.findById(req.params.id);
+    const review = new Review(req.body.review);
+    foodtruck.reviews.push(review);
+    await review.save();
+    await foodtruck.save();
+    res.redirect(`/foodtruck/${foodtruck._id}`);
+  })
+);
+
+app.delete(
+  "/foodtruck/:id/review/:reviewId",
+  CatchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Foodtruck.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/foodtruck/${id}`);
+  })
+);
+
+app.all("*", (req, res, next) => {
+  next(new AppError("Not Found", 404));
 });
 
-app.get("/foodtruck/:id", async (req, res) => {
-  const { id } = req.params;
-  const foodtruck = await Foodtruck.findById(id).populate("reviews");
-  res.render("foodtrucks/show", { foodtruck });
-});
-
-app.get("/foodtruck/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const foodtruck = await Foodtruck.findById(id);
-  res.render("foodtrucks/edit", { foodtruck });
-});
-
-app.put("/foodtruck/:id", async (req, res) => {
-  const { id } = req.params;
-  const updateFoodtruck = await Foodtruck.findByIdAndUpdate(id, {
-    ...req.body.foodtruck,
-  });
-  res.redirect(`/foodtruck/${updateFoodtruck._id}`);
-});
-
-app.delete("/foodtruck/:id", async (req, res) => {
-  const { id } = req.params;
-  await Foodtruck.findByIdAndDelete(id);
-  res.redirect("/foodtruck");
-});
-
-app.post("/foodtruck/:id/review", async (req, res) => {
-  const foodtruck = await Foodtruck.findById(req.params.id);
-  const review = new Review(req.body.review);
-  foodtruck.reviews.push(review);
-  await review.save();
-  await foodtruck.save();
-  res.redirect(`/foodtruck/${foodtruck._id}`);
-});
-
-app.delete("/foodtruck/:id/review/:reviewId", async (req, res) => {
-  const { id, reviewId } = req.params;
-  await Foodtruck.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-  await Review.findByIdAndDelete(reviewId);
-  res.redirect(`/foodtruck/${id}`);
+app.use((err, req, res, next) => {
+  const { status = 500 } = err;
+  if (!err.message) {
+    err.message = "Something Wrong";
+  }
+  res.status(status).render("error", { err });
 });
 
 app.listen(3000, () => {
