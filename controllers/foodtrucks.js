1b910aca52689890554c1aa4bc654b7b9b6f33df
hanifomarr <57039713+hanifomarr@users.noreply.github.com>
2023-04-07 12:54:18 +0800

    Foodtruck controllers

---
+++ controllers/foodtrucks.js
@@ -0,0 +1,56 @@
+const Foodtruck = require("../models/foodtruck");
+
+module.exports.index = async (req, res) => {
+  const foodtrucks = await Foodtruck.find({});
+  res.render("foodtrucks/index", { foodtrucks });
+};
+
+module.exports.createFormFoodtruck = (req, res) => {
+  res.render("foodtrucks/new");
+};
+
+module.exports.createFoodtruck = async (req, res, next) => {
+  const newFoodtruck = new Foodtruck(req.body.foodtruck);
+  newFoodtruck.author = req.user._id;
+  await newFoodtruck.save();
+  req.flash("success", "Successfully added");
+  res.redirect(`/foodtruck/${newFoodtruck._id}`);
+};
+
+module.exports.getFoodtruck = async (req, res) => {
+  const { id } = req.params;
+  const foodtruck = await Foodtruck.findById(id)
+    .populate({ path: "reviews", populate: { path: "author" } })
+    .populate("author");
+  if (!foodtruck) {
+    req.flash("error", "Foodtruck not found");
+    return res.redirect("/foodtruck");
+  }
+  res.render("foodtrucks/show", { foodtruck });
+};
+
+module.exports.showEditFoodtruck = async (req, res) => {
+  const { id } = req.params;
+  const foodtruck = await Foodtruck.findById(id);
+  if (!foodtruck) {
+    req.flash("error", "Foodtruck Not found");
+    return res.redirect(`/foodtruck`);
+  }
+  res.render("foodtrucks/edit", { foodtruck });
+};
+
+module.exports.editFoodtruck = async (req, res) => {
+  const { id } = req.params;
+  const updateFoodtruck = await Foodtruck.findByIdAndUpdate(id, {
+    ...req.body.foodtruck,
+  });
+  req.flash("success", "Successfully updated");
+  res.redirect(`/foodtruck/${id}`);
+};
+
+module.exports.deleteFoodtrucks = async (req, res) => {
+  const { id } = req.params;
+  await Foodtruck.findByIdAndDelete(id);
+  req.flash("success", "Successfully deleted");
+  res.redirect("/foodtruck");
+};
--- routes/foodtrucks.js
+++ routes/foodtrucks.js
@@ -1,64 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const CatchAsync = require("../utils/CatchAsync");
-const Foodtruck = require("../models/foodtruck");
 const { isLoggedIn, isAuhtor, validateFoodtruck } = require("../middleware");
-const AppError = require("../utils/AppError");
-const { foodtruckSchema } = require("../schemas");
+const foodtrucks = require("../controllers/foodtrucks");
 
-router.get(
-  "/",
-  CatchAsync(async (req, res) => {
-    const foodtrucks = await Foodtruck.find({});
-    res.render("foodtrucks/index", { foodtrucks });
-  })
-);
+router.get("/", CatchAsync(foodtrucks.index));
 
-router.get("/new", isLoggedIn, (req, res) => {
-  res.render("foodtrucks/new");
-});
+router.get("/new", isLoggedIn, foodtrucks.createFormFoodtruck);
 
 router.post(
   "/",
   isLoggedIn,
   validateFoodtruck,
-  CatchAsync(async (req, res, next) => {
-    const newFoodtruck = new Foodtruck(req.body.foodtruck);
-    newFoodtruck.author = req.user._id;
-    await newFoodtruck.save();
-    req.flash("success", "Successfully added");
-    res.redirect(`/foodtruck/${newFoodtruck._id}`);
-  })
+  CatchAsync(foodtrucks.createFoodtruck)
 );
 
-router.get(
-  "/:id",
-  CatchAsync(async (req, res) => {
-    const { id } = req.params;
-    const foodtruck = await Foodtruck.findById(id)
-      .populate({ path: "reviews", populate: { path: "author" } })
-      .populate("author");
-    if (!foodtruck) {
-      req.flash("error", "Foodtruck not found");
-      return res.redirect("/foodtruck");
-    }
-    res.render("foodtrucks/show", { foodtruck });
-  })
-);
+router.get("/:id", CatchAsync(foodtrucks.getFoodtruck));
 
 router.get(
   "/:id/edit",
   isLoggedIn,
   isAuhtor,
-  CatchAsync(async (req, res) => {
-    const { id } = req.params;
-    const foodtruck = await Foodtruck.findById(id);
-    if (!foodtruck) {
-      req.flash("error", "Foodtruck Not found");
-      return res.redirect(`/foodtruck`);
-    }
-    res.render("foodtrucks/edit", { foodtruck });
-  })
+  CatchAsync(foodtrucks.showEditFoodtruck)
 );
 
 router.put(
@@ -66,26 +29,14 @@ router.put(
   isLoggedIn,
   isAuhtor,
   validateFoodtruck,
-  CatchAsync(async (req, res) => {
-    const { id } = req.params;
-    const updateFoodtruck = await Foodtruck.findByIdAndUpdate(id, {
-      ...req.body.foodtruck,
-    });
-    req.flash("success", "Successfully updated");
-    res.redirect(`/foodtruck/${id}`);
-  })
+  CatchAsync(foodtrucks.editFoodtruck)
 );
 
 router.delete(
   "/:id",
   isLoggedIn,
   isAuhtor,
-  CatchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Foodtruck.findByIdAndDelete(id);
-    req.flash("success", "Successfully deleted");
-    res.redirect("/foodtruck");
-  })
+  CatchAsync(foodtrucks.deleteFoodtrucks)
 );
 
 module.exports = router;
