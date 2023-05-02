1b2930cfa8fcb9aa719d923f2bd40fdc1ee82455
Hanif Omar <57039713+hanifomarr@users.noreply.github.com>
2023-05-03 01:36:59 +0800

    setPopup and map styling

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,10 +1,15 @@
 mapboxgl.accessToken = mapBoxToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v12", // style URL
+  style: "mapbox://styles/mapbox/light-v11", // style URL
   center: foodtruck.geometry.coordinates, // starting position [lng, lat]
   zoom: 13, // starting zoom
 });
 
 // Create a default Marker and add it to the map.
-const marker1 = new mapboxgl.Marker().setLngLat(foodtruck.geometry.coordinates).addTo(map);
+const marker1 = new mapboxgl.Marker()
+  .setLngLat(foodtruck.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25})
+      .setHTML(`<h3>${foodtruck.location}</h3>`))
+  .addTo(map);
